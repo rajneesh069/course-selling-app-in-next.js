881b5ab2745ea53b9480736cfdd1918cacd3384b
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-12-20 15:47:34 +0530

    Fixed the background colour of the landing page for now, will add Dark Mode that will solve the black background problem in future I guess

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body style={{backgroundColor : "white"}}>
+      <body style={{backgroundColor : "white", height : "100vh"}}>
         <Main />
         <NextScript />
       </body>
